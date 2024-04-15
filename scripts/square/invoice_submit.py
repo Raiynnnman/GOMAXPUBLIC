7ecfb83db353a8edeee182b050190fdc1eaf5d12
Paul Maszy <paulmaszy@gmail.com>
2024-04-15 18:36:56 -0500

    Updated invoice numbers to conserve ids

--- scripts/square/invoice_submit.py
+++ scripts/square/invoice_submit.py
@@ -69,8 +69,24 @@ inv = db.query(q,p)
 
 minv = db.query("""
     select count(id) as a from invoices
+    where invoice_status_id>=5
     """)
+t = str(minv)
 minv = minv[0]['a']
+
+while True:
+    b = db.query("""
+        select * from stripe_invoice_status
+            where stripe_invoice_number = %s
+        """,(str(minv).zfill(7),)
+        )
+    if len(b) < 1:
+        minv = int(minv)
+        break
+    minv = int(minv) + 1
+
+print("minv=%s" % minv)
+
 if config.getKey("environment") != "prod":
     minv += 10000
 print(inv)
