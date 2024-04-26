f3c8b5642b25e877bfa6e5378f82be2df167445b
Paul Maszy <paulmaszy@gmail.com>
2024-04-26 09:13:30 -0500

    Removed buffer bottom

--- ui/src/pain/landing/RegisterProvider.js
+++ ui/src/pain/landing/RegisterProvider.js
@@ -656,9 +656,6 @@ class RegisterProvider extends Component {
                 
             </div>
             )}
-        {(this.props.match.path.includes("short")) && (
-        <div style={{height:"100px",backgroundColor:"black"}}></div>
-        )}
         {(!this.props.match.path.includes("short")) && (
         <div style={{height:"500px",backgroundColor:"black"}}></div>
         )}
