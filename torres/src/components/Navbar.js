3aff61dbdd436b1f5c60c60811f08194c30057cf
Paul Maszy <paulmaszy@gmail.com>
2024-05-25 18:27:21 -0500

    Handled menu start

--- torres/src/components/Navbar.js
+++ torres/src/components/Navbar.js
@@ -1,15 +1,34 @@
 import React ,  { Component } from "react";
 import { connect } from 'react-redux';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 class Navbar extends Component {
     constructor(props) { 
         super(props);
+        this.state = { 
+            menu: false,
+            menuAnchor: null
+        } 
         this.dologin = this.dologin.bind(this);
+        this.handleClose = this.handleClose.bind(this);
         this.doregister = this.doregister.bind(this);
+        this.toggleMenu = this.toggleMenu.bind(this);
     } 
     componentDidMount() { 
     } 
     componentWillMount() { 
+    } 
+    toggleMenu(e) { 
+        this.state.menu = !this.state.menu 
+        this.state.menuAnchor = e.currentTarget
+        this.setState(this.state)
+    } 
+    handleClose() { 
+        this.state.menu = false;
+        this.state.menuAnchor = null;
+        this.setState(this.state)
     } 
     dologin() { 
         console.log("here");
@@ -19,6 +38,7 @@ class Navbar extends Component {
         window.location = '/register';
     } 
     render(){
+        console.log("nav",this.props);
         return(
             <>
             <div className="row" style={{height:90}}>
@@ -34,6 +54,50 @@ class Navbar extends Component {
                                     </a>
                                 </div>
                             </div>
+                            {(this.props.currentUser) && (
+                                <div className="col-lg-8 d-none d-lg-block">
+                                    <div className="mainmenu-wrapper">
+                                        <nav>
+                                            <ul className="main-menu">
+                                                <li className="active"><a href="/">Home</a></li>
+                                                <li><a href="/#support">Support</a></li>
+                                                <li>
+                                                    <div style={{cursor:"pointer",color:"white"}} onClick={this.toggleMenu}>
+                                                        {this.props.currentUser.email}  
+                                                    </div>
+                                                      <Menu
+                                                        id="basic-menu"
+                                                        anchorEl={this.state.menuAnchor}
+                                                        open={this.state.menu}
+                                                        onClose={this.handleClose}
+                                                        MenuListProps={{
+                                                          'aria-labelledby': 'basic-button',
+                                                        }}
+                                                      >
+                                                        <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                                                        <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                                                        <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                                                      </Menu>
+                                                </li>
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                </div>
+                            )}
+                            {(this.props.currentUser) && (
+                            <div className="col-sm-7 col-7 d-block d-lg-none">
+                                <div className="mobile-menu">
+                                    <nav>
+                                        <ul>                              
+                                            <li className="active"><a href="/#home">Home</a></li>
+                                            <li><a href="/#support">Support</a></li>
+                                            <li><div style={{color:"white"}}>{this.props.currentUser.email}</div></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div>
+                            )}
+                            {(!this.props.currentUser) && (
                             <div className="col-lg-8 d-none d-lg-block">
                                 <div className="mainmenu-wrapper">
                                     <nav>
@@ -50,6 +114,8 @@ class Navbar extends Component {
                                     <button onClick={this.doregister} style={{marginLeft:10}} className="button-default button-olive" type="button">Register</button>
                                 </div>
                             </div>
+                            )}
+                            {(!this.props.currentUser) && (
                             <div className="col-sm-7 col-7 d-block d-lg-none">
                                 <div className="mobile-menu">
                                     <nav>
@@ -66,6 +132,7 @@ class Navbar extends Component {
                                     <button onClick={this.doregister} style={{marginLeft:10}} className="button-default button-olive" type="button">Register</button>
                                 </div>
                             </div>
+                            )}
                         </div>
                     </div>
                 </div>
--- torres/src/pain/dashboard/components/TrendHeroWithStats.js
+++ torres/src/pain/dashboard/components/TrendHeroWithStats.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
 import cx from 'classnames';
 import classnames from 'classnames';
 import translate from '../../utils/translate';
@@ -26,10 +27,12 @@ class TrendHeroWithStats extends Component {
             <div className="pb-xlg h-100">
               <Card
                 className="mb-0 h-100"
-                style={{border:"1px solid #e3e3e3",borderRadius:"10px",boxShadow:"rgba(0, 0, 0, 0.15) 0px 5px 15px 0px"}}
+                style={{margin:10,border:"1px solid #e3e3e3",borderRadius:"10px",boxShadow:"rgba(0, 0, 0, 0.15) 0px 5px 15px 0px"}}
                 bodyClass="mt-lg"
-                title={<h5>{translate(this.props.title)}</h5>}
               >
+                <CardHeader title={<h5>{translate(this.props.title)}</h5>}></CardHeader>
+                <hr/>
+                <CardContent>
                   <div style={{marginBottom:10}}> 
                     <div style={{height:30,display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
                       <font style={{fontSize: '40px',fontWeight:"bold"}}>
@@ -64,6 +67,7 @@ class TrendHeroWithStats extends Component {
                           </p>
                       </div>
                   </div>
+                </CardContent>
               </Card>
             </div>
         </>
