2800312b15ae2acc9894719503c6288624d578b4
Paul Maszy <paulmaszy@gmail.com>
2024-06-20 09:13:11 -0500

    Switch to token

--- backend/processing/Registrations.py
+++ backend/processing/Registrations.py
@@ -692,7 +692,7 @@ class RegisterProvider(RegistrationsBase):
             if BS == 1:
                 cust_id = params['cust_id']
                 card = params['card']['token']
-                tok = card['card']['id']
+                tok = card['id']
                 l = db.query("""
                     select stripe_key from setupIntents where uuid=%s
                 """,(cust_id,))
