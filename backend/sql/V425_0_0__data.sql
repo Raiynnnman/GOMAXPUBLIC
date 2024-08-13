5295506f3735a1458fd74d9d4cfde949ec287071
Paul Maszy <paulmaszy@gmail.com>
2024-08-13 12:40:49 +0000

    Dont delete from office_emails

--- backend/sql/V425_0_0__data.sql
+++
@@ -1,4 +0,0 @@
-
-create index office_emails_index on office_emails(email);
-delete from office_emails where email in (select email from office);
-delete from office_emails where email in (select email from users);
