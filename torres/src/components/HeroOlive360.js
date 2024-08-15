a35c7e9d00d81e36b3cfec4eba5b014dab332a67
Paul Maszy <paulmaszy@gmail.com>
2024-08-15 16:04:09 +0000

    Updates to main page for 360

--- torres/src/components/HeroOlive360.js
+++ torres/src/components/HeroOlive360.js
@@ -12,10 +12,10 @@ class HeroOlive extends Component {
                             <div className="banner-text" style={{fontFamily:'roboto'}}>
                                 <div className="banner-table-cell" style={{fontFamily:'roboto'}}>
                                     <h1 style={{color:"white",fontFamily:'roboto'}}>360 Blu Consulting</h1>
-                                    <h3 style={{color:"white",fontFamily:'roboto'}}>360 Blu Consulting is globally focused on helping organizations operate effectively. A management consultant, or management analyst, provides an outside perspective on problem-solving, best practices, and strategy to help companies improve their performance.</h3>
+                                    <h3 style={{color:"white",fontFamily:'roboto'}}>360 Blu Consulting is globally focused on helping organizations operate effectively.</h3>
                                     <p style={{color:"white",fontFamily:'roboto'}}>
-When it comes to achieving sustainable growth, maximizing profitability, and delivering value, no one does it quite like Simon-Kucher. We specialize in business consulting services aimed at driving measurable revenue and profit for our clients over the long term.
-Our expertise lies in optimizing various aspects of commercial strategy, such as product innovation, pricing and revenue management , marketing, and sales , to ensure sustainable growth and profitability for your company.
+When it comes to achieving sustainable growth, maximizing profitability, and delivering value, no one does it quite like 360BluConsulting. We specialize in business consulting services aimed at driving measurable revenue and profit for our clients over the long term.
+Our expertise lies in optimizing various aspects of commercial strategy, such as product innovation, pricing and revenue management, marketing, and sales, to ensure sustainable growth and profitability for your company.
                                     </p>
                                 </div>
                             </div>
