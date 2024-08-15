6bad30b28821d5a3fea187a129686411c9d3e7a5
Paul Maszy <paulmaszy@gmail.com>
2024-08-15 16:23:47 +0000

    Updated content

--- torres/src/components/HeroOlive360.js
+++ torres/src/components/HeroOlive360.js
@@ -11,11 +11,13 @@ class HeroOlive extends Component {
                         <div className="col-lg-6 h-100">
                             <div className="banner-text" style={{fontFamily:'roboto'}}>
                                 <div className="banner-table-cell" style={{fontFamily:'roboto'}}>
-                                    <h1 style={{color:"white",fontFamily:'roboto'}}>360BluConsulting</h1>
+                                    <h1 style={{color:"white",fontFamily:'roboto'}}><font style={{color:'#153cb5'}}>360BluConsulting</font></h1>
                                     <h3 style={{color:"white",fontFamily:'roboto'}}>
+                                        <div style={{margin:10}}>
 360BluConsulting is globally focused on helping organizations operate effectively.
 When it comes to achieving sustainable growth, maximizing profitability, and delivering value, no one does it quite like 360BluConsulting. We specialize in business consulting services aimed at driving measurable revenue and profit for our clients over the long term.
 Our expertise lies in optimizing various aspects of commercial strategy, such as product innovation, pricing and revenue management, marketing, and sales, to ensure sustainable growth and profitability for your company.
+                                        </div>
                                     </h3>
                                 </div>
                             </div>
