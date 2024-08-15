9266b42714a5f7d22c4cf4ff96208c2a414ece3d
Paul Maszy <paulmaszy@gmail.com>
2024-08-15 16:07:07 +0000

    Updated paragraph

--- torres/src/components/HeroOlive360.js
+++ torres/src/components/HeroOlive360.js
@@ -11,44 +11,14 @@ class HeroOlive extends Component {
                         <div className="col-lg-6 h-100">
                             <div className="banner-text" style={{fontFamily:'roboto'}}>
                                 <div className="banner-table-cell" style={{fontFamily:'roboto'}}>
-                                    <h1 style={{color:"white",fontFamily:'roboto'}}>360 Blu Consulting</h1>
-                                    <h3 style={{color:"white",fontFamily:'roboto'}}>360 Blu Consulting is globally focused on helping organizations operate effectively.</h3>
-                                    <p style={{color:"white",fontFamily:'roboto'}}>
+                                    <h1 style={{color:"white",fontFamily:'roboto'}}>360BluConsulting</h1>
+                                    <h3 style={{color:"white",fontFamily:'roboto'}}>
+360BluConsulting is globally focused on helping organizations operate effectively.
 When it comes to achieving sustainable growth, maximizing profitability, and delivering value, no one does it quite like 360BluConsulting. We specialize in business consulting services aimed at driving measurable revenue and profit for our clients over the long term.
 Our expertise lies in optimizing various aspects of commercial strategy, such as product innovation, pricing and revenue management, marketing, and sales, to ensure sustainable growth and profitability for your company.
-                                    </p>
+                                    </h3>
                                 </div>
                             </div>
-
-                            {(false) && (
-                            <>
-                            <div className="banner-apps"> 
-                                <div className="single-app">
-                                    <div className="single-app-table-cell">
-                                        <i className="zmdi zmdi-apple"></i>
-                                        <h4>ios</h4>
-                                        <h3>102K</h3>
-                                    </div>
-                                </div>
-
-                                <div className="single-app">
-                                    <div className="single-app-table-cell">
-                                        <i className="zmdi zmdi-cloud-download"></i>
-                                        <h4>Download</h4>
-                                        <h3>102K</h3>
-                                    </div>
-                                </div>
-
-                                <div className="single-app">
-                                    <div className="single-app-table-cell">
-                                        <i className="zmdi zmdi-android"/>
-                                        <h4>Android</h4>
-                                        <h3>102K</h3>
-                                    </div>
-                                </div>
-                            </div>
-                            </>
-                            )}
                         </div>
                     </div>
                 </div>
