a36abbad15aacd70ea8002cae266e6325cceb649
NBAJay4KT <jay@gomaxpain.com>
2025-10-07 12:53:37 -0700

    Delete maxpain directory

--- maxpain/composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/list/LoginState.kt
+++
@@ -1,8 +0,0 @@
-package com.jetbrains.kmpapp.data
-
-sealed class LoginState {
-    data object Idle : LoginState()
-    data object Loading : LoginState()
-    data object Success : LoginState()
-    data class Error(val message: String) : LoginState()
-}
